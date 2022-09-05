2f70a6dd518099a21732e987fa8f03d517081d5a
yasirse <yasir.arfatse@gmail.com>
2022-09-05 07:21:26 +0500

    Update pcard.blade.php

--- resources/views/pcard.blade.php
+++ resources/views/pcard.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/css/app.css')
 </head>
 <body class="h-screen bg-gradient bg-gradient-to-br from-theme-blue to-theme-pink flex items-center justify-center">
-<div class="box-wrapper border border-gray-300 w-1/2 h-4/5 p-10 relative ">
+<div class="box-wrapper border border-gray-300 sm:w-1/2 sm:h-4/5 sm:p-10 relative w-full h-full">
     <div class=" w-6 h-6 border border-gray-100 rounded-full absolute top-0 left-0"></div>
     <div class=" w-10 h-10   absolute top-0 right-0">
         <div class=" w-full h-full rounded-full border border-gray-100  absolute top-0 left-0"> 
@@ -16,13 +16,13 @@
         </div>
     </div>
     <!-- card section-->
-    <div class="card bg-white w-full h-full rounded-2xl flex  shadow-theme-1 overflow-hidden">
+    <div class="card bg-white w-full h-full rounded-2xl flex flex-wrap  shadow-theme-1 overflow-hidden p-5 sm:p-0">
        <!-- left side-->
-        <div class="left-side w-1/2 bg-gradient-to-tr from-theme-pink to-theme-blue   rounded-2xl scale-105 shadow-theme-2">
+       {{--  <div class="left-side  bg-gradient-to-tr from-theme-pink to-theme-blue w-full   rounded-2xl scale-105 shadow-theme-2">
             <div class="log w-20 text-white absolute left-6 top-0">   
                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.013 512.013"><path d="M287.757 225.794l-31.808 8.352-88.256 23.104c-31.168 7.712-58.432 17.984-84.704 8.992-35.712-15.424-35.072-55.552-8.448-98.24C27.469 206.85-57.235 331.074 57.325 343.618c14.592 1.92 40.576-3.2 68.8-15.072l129.824-53.28 256.064-104.704-224.256 55.232z"/></svg>
             </div>
-            <div class="number absolute -right-10 top-0 mt-8 bg-gradient bg-gradient-to-bl from-theme-blue to-theme-pink text-transparent bg-clip-text text-shadow">
+            <div class="number absolute right-0 sm:-right-10 top-0 mt-8 bg-gradient bg-gradient-to-bl from-theme-blue to-theme-pink text-transparent bg-clip-text text-shadow">
                 <span class="text-10xl font-bold leading-none font-sans">32</span>
             </div>
             <div class="shoe absolute bottom-12 left-0">
@@ -40,15 +40,21 @@
                 <span class="w-4 h-4 rounded-full bg-gray-300 hover:bg-gray-300 block"></span>
                 
             </div>
-        </div>
+        </div> --}}
         <!-- right side-->
-,       <div class="right-side w-1/2  bg-orange-300 ">
+,       {{-- <div class="right-side   bg-orange-300 w-full ">
             <div class="tex-logo flex justify-end ">
                 <img src="/images/text-logo.png" alt="" >
             </div>
-        </div>
+            <div class="price">
+                <span class="font-bold text-3xl text-gray-600">$99</span>
+                <p class=" md:text-xl sm:text-sm text-gray-700 ">   Experience sports, traning, shopping and everything else that's new at Nike from any country in the world.</p>            </div>
+            </div>
+        --}}
+        <div class="yasir w-full bg-green-100"><p>fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p></div>
+        <div class="yasir w-full bg-black-100">gghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhf</div>
+    </div> 
     
-    </div>
 </div>    
 
 </body>
